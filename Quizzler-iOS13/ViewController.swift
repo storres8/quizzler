430b2ef4c55addcf72ce925b7067efb0bee47d63
Steven Torres <stevetorres7562@gmail.com>
2020-07-16 11:04:15 -0400

    Fixed bugs

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -8,13 +8,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        ["Four + Two is equal to Six", "True"],
+        ["Five - Three is greater than One", "True"],
+        ["Three + Eight is less than Ten", "False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        questionLabel.text = "Four + Two is equal to Six"
+        updateUI()
     }
-
+    
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle // True or False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("correct")
+        } else {
+            print("wrong")
+        }
+        
+        if questionNumber < 2 {
+            questionNumber += 1
+            updateUI()
+        } else {
+            questionNumber = 0
+            updateUI()
+        }
+        
+    }
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 }
